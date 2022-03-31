ed9720539a8e57890106c9a095e62ec4d5cfa6c7
Jireh1226 <katebejireh@gmail.com>
2022-03-31 13:46:44 -0500

    amy planning scene stuff

---
+++ planning_scene_ros_api/src/planning_scene_ros_api_tutorial.cpp
@@ -0,0 +1,294 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2012, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Sachin Chitta, Michael Lautman */
+
+#include <ros/ros.h>
+#include <geometry_msgs/Pose.h>
+
+// MoveIt
+#include <moveit_msgs/PlanningScene.h>
+#include <moveit_msgs/AttachedCollisionObject.h>
+#include <moveit_msgs/GetStateValidity.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/ApplyPlanningScene.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+
+#include <moveit/robot_model_loader/robot_model_loader.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit/robot_state/conversions.h>
+
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "planning_scene_ros_api_tutorial");
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  ros::NodeHandle node_handle;
+
+
+  // Initialising and defining the planning group for move_base
+  static const std::string PLANNING_GROUP = "xarm";
+  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+  // Raw pointers are frequently used to refer to the planning group for improved performance.
+  const robot_state::JointModelGroup *joint_model_group = move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+  // Initialising the PlanninSceneInterface that enables taking
+  // collision objects into account during planning
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+  // BEGIN_TUTORIAL
+  //
+  // Visualization
+  // ^^^^^^^^^^^^^
+  // The package MoveItVisualTools provides many capabilties for visualizing objects, robots,
+  // and trajectories in RViz as well as debugging tools such as step-by-step introspection of a script
+  moveit_visual_tools::MoveItVisualTools visual_tools("panda_link0");
+  visual_tools.deleteAllMarkers();
+
+  ros::Publisher planning_scene_diff_publisher = node_handle.advertise<moveit_msgs::PlanningScene>("planning_scene", 1);
+  ros::WallDuration sleep_t(0.5);
+  while (planning_scene_diff_publisher.getNumSubscribers() < 1)
+  {
+    sleep_t.sleep();
+  }
+
+
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+  // Define a collision object ROS message.
+  moveit_msgs::CollisionObject collision_object;
+  collision_object.header.frame_id = move_group.getPlanningFrame();
+
+  // The id of the object is used to identify it.
+  collision_object.id = "box1";
+
+  // Define a box to add to the world.
+  shape_msgs::SolidPrimitive primitive2;
+  primitive2.type = primitive2.BOX;
+  primitive2.dimensions.resize(3);
+  primitive2.dimensions[0] = 2.0;
+  primitive2.dimensions[1] = 2.0;
+  primitive2.dimensions[2] = 2.0;
+
+  // Define a pose for the box (specified relative to frame_id)
+  geometry_msgs::Pose box_pose;
+  box_pose.orientation.w = 1.0;
+  box_pose.position.x = 0.25;
+  box_pose.position.y = -0.15;
+  box_pose.position.z = 0.3;
+
+  collision_object.primitives.push_back(primitive2);
+  collision_object.primitive_poses.push_back(box_pose);
+  collision_object.operation = collision_object.ADD;
+
+  std::vector<moveit_msgs::CollisionObject> collision_objects;
+  collision_objects.push_back(collision_object);
+
+  // Now, let's add the collision object into the world
+  ROS_INFO("TEST TEST TEST");
+  //moveit::planning_interface::PlanningSceneInterface planning_scene1;
+  planning_scene_interface.addCollisionObjects(collision_objects);
+  //planning_scene.is_diff = true;
+  //planning_scene_diff_publisher.publish(planning_scene1);
+
+
+
+
+  // ROS API
+  // ^^^^^^^
+  // The ROS API to the planning scene publisher is through a topic interface
+  // using "diffs". A planning scene diff is the difference between the current
+  // planning scene (maintained by the move_group node) and the new planning
+  // scene desired by the user.
+  //
+  // Advertise the required topic
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  // We create a publisher and wait for subscribers
+  // Note that this topic may need to be remapped in the launch file
+  // ros::Publisher planning_scene_diff_publisher = node_handle.advertise<moveit_msgs::PlanningScene>("planning_scene", 1);
+  // ros::WallDuration sleep_t(0.5);
+  // while (planning_scene_diff_publisher.getNumSubscribers() < 1)
+  // {
+  //   sleep_t.sleep();
+  // }
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
+
+  // Define the attached object message
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  // We will use this message to add or
+  // subtract the object from the world
+  // and to attach the object to the robot
+  moveit_msgs::AttachedCollisionObject attached_object;
+  attached_object.link_name = "panda_leftfinger";
+  /* The header must contain a valid TF frame*/
+  attached_object.object.header.frame_id = "panda_leftfinger";
+  /* The id of the object */
+  attached_object.object.id = "box";
+
+  /* A default pose */
+  geometry_msgs::Pose pose;
+  pose.orientation.w = 1.0;
+
+  /* Define a box to be attached */
+  shape_msgs::SolidPrimitive primitive;
+  primitive.type = primitive.BOX;
+  primitive.dimensions.resize(3);
+  primitive.dimensions[0] = 0.1;
+  primitive.dimensions[1] = 0.1;
+  primitive.dimensions[2] = 0.1;
+
+  attached_object.object.primitives.push_back(primitive);
+  attached_object.object.primitive_poses.push_back(pose);
+
+  // Note that attaching an object to the robot requires
+  // the corresponding operation to be specified as an ADD operation
+  attached_object.object.operation = attached_object.object.ADD;
+
+  // Since we are attaching the object to the robot hand to simulate picking up the object,
+  // we want the collision checker to ignore collisions between the object and the robot hand
+  attached_object.touch_links = std::vector<std::string>{ "panda_hand", "panda_leftfinger", "panda_rightfinger" };
+
+  // Add an object into the environment
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  // Add the object into the environment by adding it to
+  // the set of collision objects in the "world" part of the
+  // planning scene. Note that we are using only the "object"
+  // field of the attached_object message here.
+  ROS_INFO("Adding the object into the world at the location of the hand.");
+  moveit_msgs::PlanningScene planning_scene;
+  planning_scene.world.collision_objects.push_back(attached_object.object);
+  planning_scene.is_diff = true;
+  planning_scene_diff_publisher.publish(planning_scene);
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+
+  // Interlude: Synchronous vs Asynchronous updates
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  // There are two separate mechanisms available to interact
+  // with the move_group node using diffs:
+  //
+  // * Send a diff via a rosservice call and block until
+  //   the diff is applied (synchronous update)
+  // * Send a diff via a topic, continue even though the diff
+  //   might not be applied yet (asynchronous update)
+  //
+  // While most of this tutorial uses the latter mechanism (given the long sleeps
+  // inserted for visualization purposes asynchronous updates do not pose a problem),
+  // it would is perfectly justified to replace the planning_scene_diff_publisher
+  // by the following service client:
+  ros::ServiceClient planning_scene_diff_client =
+      node_handle.serviceClient<moveit_msgs::ApplyPlanningScene>("apply_planning_scene");
+  planning_scene_diff_client.waitForExistence();
+  // and send the diffs to the planning scene via a service call:
+  moveit_msgs::ApplyPlanningScene srv;
+  srv.request.scene = planning_scene;
+  planning_scene_diff_client.call(srv);
+  // Note that this does not continue until we are sure the diff has been applied.
+  //
+  // Attach an object to the robot
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  // When the robot picks up an object from the environment, we need to
+  // "attach" the object to the robot so that any component dealing with
+  // the robot model knows to account for the attached object, e.g. for
+  // collision checking.
+  //
+  // Attaching an object requires two operations
+  //  * Removing the original object from the environment
+  //  * Attaching the object to the robot
+
+  /* First, define the REMOVE object message*/
+  moveit_msgs::CollisionObject remove_object;
+  remove_object.id = "box";
+  remove_object.header.frame_id = "panda_link0";
+  remove_object.operation = remove_object.REMOVE;
+
+  // Note how we make sure that the diff message contains no other
+  // attached objects or collisions objects by clearing those fields
+  // first.
+  /* Carry out the REMOVE + ATTACH operation */
+  ROS_INFO("Attaching the object to the hand and removing it from the world.");
+  planning_scene.world.collision_objects.clear();
+  planning_scene.world.collision_objects.push_back(remove_object);
+  planning_scene.robot_state.attached_collision_objects.push_back(attached_object);
+  planning_scene_diff_publisher.publish(planning_scene);
+
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+
+  // Detach an object from the robot
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  // Detaching an object from the robot requires two operations
+  //  * Detaching the object from the robot
+  //  * Re-introducing the object into the environment
+
+  /* First, define the DETACH object message*/
+  moveit_msgs::AttachedCollisionObject detach_object;
+  detach_object.object.id = "box";
+  detach_object.link_name = "panda_link8";
+  detach_object.object.operation = attached_object.object.REMOVE;
+
+  // Note how we make sure that the diff message contains no other
+  // attached objects or collisions objects by clearing those fields
+  // first.
+  /* Carry out the DETACH + ADD operation */
+  ROS_INFO("Detaching the object from the robot and returning it to the world.");
+  planning_scene.robot_state.attached_collision_objects.clear();
+  planning_scene.robot_state.attached_collision_objects.push_back(detach_object);
+  planning_scene.robot_state.is_diff = true;
+  planning_scene.world.collision_objects.clear();
+  planning_scene.world.collision_objects.push_back(attached_object.object);
+  planning_scene.is_diff = true;
+  planning_scene_diff_publisher.publish(planning_scene);
+
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+
+  // Remove the object from the collision world
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  // Removing the object from the collision world just requires
+  // using the remove object message defined earlier.
+  // Note, also how we make sure that the diff message contains no other
+  // attached objects or collisions objects by clearing those fields
+  // first.
+  ROS_INFO("Removing the object from the world.");
+  planning_scene.robot_state.attached_collision_objects.clear();
+  planning_scene.world.collision_objects.clear();
+  planning_scene.world.collision_objects.push_back(remove_object);
+  planning_scene_diff_publisher.publish(planning_scene);
+  // END_TUTORIAL
+
+  visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to end the demo");
+
+  ros::shutdown();
+  return 0;
+}
